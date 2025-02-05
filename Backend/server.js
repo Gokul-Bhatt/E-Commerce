0ee74fbe5625a14e5f18be1d2d16127b0b73bd4b
gokul <142083386+Gokul-Bhatt@users.noreply.github.com>
2025-02-05 18:48:11 +0530

    routing

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const authroute = require("./router/auth_router")
 
 
-app.get("/", (req, res)=>{
-    res.status(200).send("server is working")
-});
-const PORT = 3000;
+// app.get("/", (req, res)=>{
+//     res.status(200).send("server is working")
+// });
+// const PORT = 3000;
+// app.listen(PORT,()=>{
+//     console.log(`server is running on ${PORT}`)
+// })
+
+app.use("/",authroute);
+
+const PORT = 5000;
+
 app.listen(PORT,()=>{
     console.log(`server is running on ${PORT}`)
 })
